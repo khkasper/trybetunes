0b7ca37b654b42925575d4b01cf8199783e5cc7b
Kristiano <kristianokasper@gmail.com>
2021-09-19 20:22:50 -0300

    Requirement 12

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    this.setState({ loading: true });
+    const user = await getUser();
+    this.setState({
+      loading: false,
+      user,
+    });
+  }
+
   render() {
+    const { user: { description, email, image, name }, loading } = this.state;
+    if (loading) {
+      return (
+        <div data-testid="page-profile">
+          <Header />
+          <Loading />
+        </div>
+      );
+    }
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        <main>
+          <img
+            data-testid="profile-image"
+            alt={ name }
+            src={ image }
+          />
+          <Link to="/profile/edit">
+            Editar perfil
+          </Link>
+          <p>Nome</p>
+          <p>{ name }</p>
+          <p>E-mail</p>
+          <p>{ email }</p>
+          <p>Descrição</p>
+          <p>{ description }</p>
+        </main>
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,12 +1,110 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 import Header from '../components/Header';
+import { getUser, updateUser } from '../services/userAPI';
 
 export default class ProfileEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      redirect: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    const { name, image, email, description } = await getUser();
+    this.setState({
+      loading: false,
+      name,
+      email,
+      image,
+      description,
+    });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick = () => {
+    const { name, email, description, image } = this.state;
+    const user = { name, email, description, image };
+    this.setState({ loading: true });
+    updateUser(user).then(() => this.setState({
+      redirect: true,
+    }));
+  }
+
   render() {
+    const { description, email, image, name, loading, redirect } = this.state;
+    const validate = name && email && description && image;
+    if (redirect) return <Redirect to="/profile" />;
+    if (loading) {
+      return (
+        <div data-testid="page-profile-edit">
+          <Header />
+          <Loading />
+        </div>
+      );
+    }
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        Profile Edit
+        <div>
+          <p>Nome</p>
+          <input
+            data-testid="edit-input-name"
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+          <p>Email</p>
+          <input
+            data-testid="edit-input-email"
+            type="text"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <p>Descrição</p>
+          <input
+            data-testid="edit-input-description"
+            type="text"
+            name="description"
+            value={ description }
+            onChange={ this.handleChange }
+          />
+          <p>Foto</p>
+          <input
+            data-testid="edit-input-image"
+            type="text"
+            name="image"
+            value={ image }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="edit-button-save"
+            type="button"
+            onClick={ this.handleClick }
+            disabled={ !validate }
+          >
+            Salvar
+          </button>
+        </div>
       </div>
     );
   }
